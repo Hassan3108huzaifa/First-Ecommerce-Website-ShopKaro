57ff61ade1eff3e2d0b84f51cb0544e68495a727
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2024-10-13 04:30:10 -0700

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],
+        domains: ['images.unsplash.com', 'shopkaro-hassanrj.vercel.app'],
+        unoptimized: true,
+
       },
 };
 
